b40b5c1522718d546f5c9db5dc5839eb559ca364
Homin Lee <homin.lee@suapapa.net>
2015-11-09 19:06:28 +0900

    fix example

--- example/main.go
+++ example/main.go
@@ -1,18 +1,12 @@
 package main
 
-import (
-	"fmt"
-
-	"./tm1638"
-)
+import "github.com/suapapa/go_tm1638"
 
 func main() {
-	fmt.Println("Hello world")
 	d, err := tm1638.NewTM1638(18, 23, 24)
 	if err != nil {
 		panic(nil)
 	}
 
 	d.DisplayError()
-	d.DisplayDigit(4, 1, true)
 }
